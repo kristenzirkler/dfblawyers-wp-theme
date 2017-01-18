ea2fd6108647e13074d3e66ce937df20de581a2d
Kristen Zirkler <bazirkler@gmail.com>
2017-01-17 22:35:03 -0800

    add robin & noela

--- js/theme-custom-js.js
+++ js/theme-custom-js.js
@@ -5,7 +5,7 @@ jQuery('.portfolio-item .image-box-content a').click(function(event) {
     var lawyerLink = jQuery(this).attr('href'),
 	    lawyerLink = lawyerLink.replace('portfolio-page','our-team');
 
-    if (lawyerLink.indexOf('greg') >= 0 || lawyerLink.indexOf('cheryl') >= 0 || lawyerLink.indexOf('aaron') >= 0 || lawyerLink.indexOf('felicia') >= 0) {
+    if (lawyerLink.indexOf('greg') >= 0 || lawyerLink.indexOf('cheryl') >= 0 || lawyerLink.indexOf('aaron') >= 0 || lawyerLink.indexOf('felicia') >= 0 || lawyerLink.indexOf('robin') >= 0 || lawyerLink.indexOf('noela') >= 0) {
        window.location = lawyerLink;
     } else {
 	    //do nothing
